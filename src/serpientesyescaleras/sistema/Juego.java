c03bc6488805ffc70f9e5707a1e7590fde7d9645
BrunoCoronado <bcoronado.morales@gmail.com>
2018-02-22 23:28:28 -0600

    Asignacion de turnos y rondas - *revisar codigo para cambios*
    implementacion de la asignacion de rondas y turnos, revisar el codigo, para optimizacicon y arreglo de envio de parametros

--- src/serpientesyescaleras/sistema/Juego.java
+++ src/serpientesyescaleras/sistema/Juego.java
@@ -17,6 +17,8 @@ public class Juego {
     private Gestion gestion = new Gestion();
     private ArrayList<String> jugadores;
     private Random numeroAleatorio = new Random();
+    private static int ronda = 1;
+    private String jugador = "";
         
     public static Casilla[][] tablero = new Casilla[10][10]; 
     
@@ -26,7 +28,6 @@ public class Juego {
         {
             case 1:
                 jugadores = gestion.ingresarJugadores();
-                //escalerasYSerpientes = gestion.ingresarEscalerasYSerpientes();
                 gestion.ingresarEscalerasYSerpientes();
                 iniciarJuego();
                 break;
@@ -39,25 +40,65 @@ public class Juego {
                 inicializar();
         }
     }
+    
+    public static void inicializarMatriz() {
+        for(int coordenadaX=0;coordenadaX<10;coordenadaX++){
+            for(int coordenadaY=0;coordenadaY<10;coordenadaY++){
+                if (coordenadaX==0 && coordenadaY==0) {
+                    tablero[coordenadaX][coordenadaY]= new Casilla("*","1",",2",",3","123");
+                }else if (coordenadaX==9 && coordenadaY==9) {
+                    tablero[coordenadaX][coordenadaY]= new Casilla("$"," ","  ","  ","");
+                }else{
+                    tablero[coordenadaX][coordenadaY]= new Casilla(" "," ","  ","  ","");
+                }
+            }
+        }
+    }
 
     private void iniciarJuego() {        
-        generarTablero();        
-        movimiento(AsignarTurno());
+        //generarTablero();        
+        movimiento(generarTablero());
     }
     
-    private void generarTablero(){
-        System.out.println("\nTurno del jugador\tRonda:");
+    private int AsignarTurno() {
+        switch(1+numeroAleatorio.nextInt(3)){
+            case 1:
+                jugador = jugadores.get(0);
+                return 1;
+            case 2:
+                jugador = jugadores.get(1);
+                return 2;
+            case 3:
+                jugador = jugadores.get(2);
+                return 3;
+        }
+        return 0;
+    }
+    
+    private int generarTablero(){
+        Integer turno=null;
+        if(ronda==1){
+            turno = AsignarTurno();
+        }
+        
+        System.out.println("\nTurno del jugador:"+jugador+"\tRonda:"+ronda);
         System.out.println("-----------------------------------------------------------------------");
         for (int coordenadaX=9;coordenadaX>-1;coordenadaX--) {                        
             for (int coordenadaY=9;coordenadaY>-1;coordenadaY--) {
-                    System.out.print("|"+tablero[coordenadaX][coordenadaY].getContenido()+tablero[coordenadaX][coordenadaY].getJugador1()+tablero[coordenadaX][coordenadaY].getJugador2()+tablero[coordenadaX][coordenadaY].getJugador3());
+                    System.out.print("|"+tablero[coordenadaX][coordenadaY].getContenido()+tablero[coordenadaX][coordenadaY].getJugador1()+
+                                     tablero[coordenadaX][coordenadaY].getJugador2()+tablero[coordenadaX][coordenadaY].getJugador3());
             }            
             System.out.print("|");
             System.out.print("\n-----------------------------------------------------------------------\n");
         }
+        if(turno!=null){
+            return turno;
+        }
+        return 0;
     }
 
     private void movimiento(int jugador) {
+        ronda = ronda + 1;
         switch(menu.menuMovimiento()){
             case 1:
                 moverJugador(tirarDado(), jugador);
@@ -84,130 +125,81 @@ public class Juego {
                 jugadorMovido = "1";
                 jugadoresParados = " ";
                 siguienteJugador = 2;
+                this.jugador=jugadores.get(1);
                 break;
             case 2:
                 jugadorMovido = ",2";
                 jugadoresParados = "  ";
-                siguienteJugador = 3;
+                siguienteJugador = 3;                
+                this.jugador=jugadores.get(2);
                 break;
             case 3:
                 jugadorMovido = ",3";
                 jugadoresParados = "  ";
                 siguienteJugador = 1;
+                this.jugador=jugadores.get(0);
                 break;
         }
-        System.out.println(movimiento + " "+jugador);
-        end:
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (tablero[i][j].getGeneral().contains(String.valueOf(jugador))){
-                    
-                    int distanciaAlBorde = 0;
-                    int posicionJugador = j;
-                    
-                    distanciaAlBorde = 9-posicionJugador;
-                    System.out.println(+distanciaAlBorde+""+movimiento);
-                    
-                    if(distanciaAlBorde==0){
-                        int subir = i+1;
-                                    if (movimiento>10) {                        
-                                    tablero[subir+1][j+(movimiento-10)].setMoverJugador(jugadorMovido, jugador);
-                                    String c1= tablero[i+1][i+(movimiento-10)].getGeneral()+""+String.valueOf(jugador);
-
-                                    tablero[subir+1][j+(movimiento-10)].setGeneral(c1);  
-                                    System.out.println("contenido nueva casilla"+c1);
-                                }else{
-                                    tablero[subir][movimiento-1].setMoverJugador(jugadorMovido, jugador);
-                                    String c2 = tablero[i][movimiento-1].getGeneral()+""+String.valueOf(jugador) ;
-                                    tablero[subir][movimiento-1].setGeneral(c2);
-
-                                    System.out.println("contenido en nueva casilla"+c2);
-                                }
-                        
-                        
-                    }
-                    
-                    if(distanciaAlBorde<movimiento){
-                        if(distanciaAlBorde==1){
-                            
-                            if(movimiento>11){
-                                int subir = i+1;
-                            
-                            }else{
-                            tablero[i+1][j+(movimiento-10)].setMoverJugador(jugadorMovido, jugador);
-                                    String c1= tablero[i+1][j+(movimiento-10)].getGeneral()+""+String.valueOf(jugador);
-
-                                    tablero[i+1][j+(movimiento-10)].setGeneral(c1);  
-                                    System.out.println("contenido nueva casilla"+c1);
-                            }
-                            
-                            
-                        }else{
-                        tablero[i+1][(movimiento-distanciaAlBorde)-1].setMoverJugador(jugadorMovido, jugador);
-                        String c1= tablero[i+1][(movimiento-distanciaAlBorde)-1].getGeneral()+""+String.valueOf(jugador);
-                        
-                        tablero[i+1][(movimiento-distanciaAlBorde)-1].setGeneral(c1);  
-                        }
-                        
-                        
-                    }
-                    
-                    if(distanciaAlBorde>=movimiento){
-                        tablero[i][j+movimiento].setMoverJugador(jugadorMovido, jugador);
-                        String c2 = tablero[i][j+movimiento].getGeneral()+""+String.valueOf(jugador) ;
-                        tablero[i][j+movimiento].setGeneral(c2);
-                        
-                        System.out.println("contenido en nueva casilla"+c2);
-                    }
-                    
-                    
-                    
-                    
-                    
-                    
-                    /*if ((i+movimiento)>=9) {                        
-                        tablero[i+1][j+(movimiento-10)].setMoverJugador(jugadorMovido, jugador);
-                        String c1= tablero[i+1][i+(movimiento-10)].getGeneral()+""+String.valueOf(jugador);
-                        
-                        tablero[i+1][j+(movimiento-10)].setGeneral(c1);  
-                        System.out.println("contenido nueva casilla"+c1);
-                    }else{
-                        tablero[i][j+movimiento].setMoverJugador(jugadorMovido, jugador);
-                        String c2 = tablero[i][j+movimiento].getGeneral()+""+String.valueOf(jugador) ;
-                        tablero[i][j+movimiento].setGeneral(c2);
-                        
-                        System.out.println("contenido en nueva casilla"+c2);
-                    }*/
-                    tablero[i][j].setMoverJugador(jugadoresParados, jugador);
-                    
-                    String contenido = tablero[i][j].getGeneral();
-                    System.out.println(contenido);
-                    tablero[i][j].setGeneral(contenido.replace(String.valueOf(jugador), " "));
-                    
-                    System.out.println(" "+i+" contenido"+contenido.replace(String.valueOf(jugador), " "));
-                    break end;
-                }
-            }
-        }
+        
+        algoritmoMovimiento(movimiento,jugador,jugadorMovido,jugadoresParados);
         generarTablero();      
         movimiento(siguienteJugador);        
     }
     
-    public static void inicializarMatriz() {
-        for(int coordenadaX=0;coordenadaX<10;coordenadaX++){
-            for(int coordenadaY=0;coordenadaY<10;coordenadaY++){
-                if (coordenadaX==0 && coordenadaY==0) {
-                    tablero[coordenadaX][coordenadaY]= new Casilla("*","1",",2",",3","123");
-                }else if (coordenadaX==9 && coordenadaY==9) {
-                    tablero[coordenadaX][coordenadaY]= new Casilla("$"," ","  ","  ","");
-                }else{
-                    tablero[coordenadaX][coordenadaY]= new Casilla(" "," ","  ","  ","");
+    public void algoritmoMovimiento(int movimiento,int jugador,String jugadorMovido, String jugadoresParados){
+        end:
+            for (int coordenadaY = 0; coordenadaY < 10; coordenadaY++) {
+                for (int coordenadaX = 0; coordenadaX < 10; coordenadaX++) {
+                    if (tablero[coordenadaY][coordenadaX].getGeneral().contains(String.valueOf(jugador))){                              
+                        int distanciaAlBorde = 9-coordenadaX;
+                        int subir = coordenadaY+1;
+                        if(distanciaAlBorde==0){
+                            if (movimiento>10) {                                                   
+                                ejecutarMovimiento(movimiento-10, subir+1,jugadorMovido,jugador);                                    
+                                organizarCasilla(coordenadaX, coordenadaY, jugadoresParados, jugador);
+                                break end;
+                            }else{                                        
+                                ejecutarMovimiento(movimiento-1, subir,jugadorMovido,jugador);
+                                organizarCasilla(coordenadaX, coordenadaY, jugadoresParados, jugador);
+                                break end;
+                            }
+                        }                    
+                        if(distanciaAlBorde<movimiento){
+                            if(distanciaAlBorde==1){
+                                if(movimiento>11){
+                                    ejecutarMovimiento(movimiento-10, subir+1,jugadorMovido,jugador);
+                                    organizarCasilla(coordenadaX, coordenadaY, jugadoresParados, jugador);
+                                    break end;
+                                }else{
+
+                                    ejecutarMovimiento(coordenadaX+(movimiento-10), subir,jugadorMovido,jugador);
+                                    organizarCasilla(coordenadaX, coordenadaY, jugadoresParados, jugador);
+                                    break end;
+                                }  
+                            }else{
+                                ejecutarMovimiento((movimiento-distanciaAlBorde)-1, coordenadaY+1,jugadorMovido,jugador);
+                                organizarCasilla(coordenadaX, coordenadaY, jugadoresParados, jugador);
+                                break end;
+                            }
+                        }
+                        if(distanciaAlBorde>=movimiento){
+                            ejecutarMovimiento(coordenadaX+movimiento, coordenadaY,jugadorMovido,jugador);
+                            organizarCasilla(coordenadaX, coordenadaY, jugadoresParados, jugador);
+                            break end;
+                        }
+                    }
                 }
             }
-        }
+                        
     }
-
-    private int AsignarTurno() {
-        return (1+numeroAleatorio.nextInt(3));
+    
+    public void ejecutarMovimiento(int coordenadaX, int coordenadaY,String jugadorMovido,int jugador){
+         tablero[coordenadaY][coordenadaX].setMoverJugador(jugadorMovido, jugador);
+        tablero[coordenadaY][coordenadaX].setGeneral(tablero[coordenadaY][coordenadaX].getGeneral()+""+String.valueOf(jugador));  
+    }
+    
+    public void organizarCasilla(int coordenadaX, int coordenadaY,String jugadorParado,int jugador){
+        tablero[coordenadaY][coordenadaX].setMoverJugador(jugadorParado, jugador);
+        tablero[coordenadaY][coordenadaX].setGeneral((tablero[coordenadaY][coordenadaX].getGeneral()).replace(String.valueOf(jugador), " "));  
     }
 }
